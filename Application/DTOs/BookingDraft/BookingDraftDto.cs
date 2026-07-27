0039a2481b11c29d440ffb8f679cae949f10a00c
DaiLT <letandai1304@gmail.com>
2026-07-27 13:59:42 +0700

    Add TotalDurationMinutes attribute into Booking

--- Application/DTOs/BookingDraft/BookingDraftDto.cs
+++ Application/DTOs/BookingDraft/BookingDraftDto.cs
@@ -17,6 +17,7 @@ namespace Application.DTOs.BookingDraft
         public double? Lng { get; set; }
         public BookingScheduledType ScheduledType { get; set; }
         public DateTime? ScheduledAt { get; set; }
+        public int? TotalDurationMinutes { get; set; }
         public Guid? WorkerProfileId { get; set; }
         public bool AutoMatch { get; set; }
     }
